edd63a8665a52f986553a8a5c37624227c0edcb3
Robert França <robertalan.dsf@gmail.com>
2024-08-07 00:09:25 -0300

    Teste de Commit.
    Testando um commit para explicação.

--- AcervoLivros/src/AcervoLiterarioApp.java
+++ AcervoLivros/src/AcervoLiterarioApp.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 public class AcervoLiterarioApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new MainScreen().setVisible(true));
-        
+        System.out.println("Teste commit");
     }
 }
 

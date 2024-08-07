57c5c4f85c3d04f9656386c2320c61c6f49eefb6
Robert França <robertalan.dsf@gmail.com>
2024-08-06 23:13:04 -0300

    Pull test.
    Testando um Pull em nosso repo.

--- AcervoLivros/src/AcervoLiterarioApp.java
+++ AcervoLivros/src/AcervoLiterarioApp.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 public class AcervoLiterarioApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new MainScreen().setVisible(true));
+        System.out.println("hello World");
     }
 }
 
